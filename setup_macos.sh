eb3a0c6ceafae69ecc829b7fe003f3aab3fc445a
Nick Adams <22064078+nickkadams@users.noreply.github.com>
2023-10-24 06:31:36 -0400

    Update Dock settings

--- setup_macos.sh
+++ setup_macos.sh
@@ -318,8 +318,8 @@ brew install homebrew/cask-drivers/logitech-options
 
 # Dock
 # delete
-defaults delete com.apple.dock persistent-apps
-defaults delete com.apple.dock persistent-others
+#defaults delete com.apple.dock persistent-apps
+#defaults delete com.apple.dock persistent-others
 
 # add
 defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Google Chrome.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
